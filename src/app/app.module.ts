66ce1bc232ea663bd48ae4e375c3db42fb1a81eb
Halan <halanbacca@outlook.com>
2022-12-01 16:27:26 -0300

    item-card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { ItemCardComponent } from './components/item-card/item-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ItemCardComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/item-card/item-card.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'philips-item-card',
+  templateUrl: './item-card.component.html',
+  styleUrls: ['./item-card.component.css'],
+})
+export class ItemCardComponent {
+  personagem = [
+    {
+      nome: 'Nick Nelson',
+      nomeAtor: 'Kit Connor',
+      descricao: 'Golden Retriever',
+      imagem: 'https://pbs.twimg.com/media/FRsm_jnXsAIG5HM.jpg:large',
+    },
+  ];
+}
